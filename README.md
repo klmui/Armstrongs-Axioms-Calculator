238c53ff95041707f8c82d13a3b1565260d1ad3c
Kenneth Mui <klmui@wisc.edu>
2020-06-15 21:33:00 -0500

    Add relfexivity rule

---
+++ README.md
@@ -0,0 +1,2 @@
+# Armstrongs-Axioms-Calculator
+Applies the reflexivity, augmentation, and reflexivity rules to find additional functional dependencies when implementing a database.
--- script.py
+++ script.py
@@ -6,11 +6,27 @@ and transitivity rules to a given set S.
 Input: S (list of tuples). A tuple: (X, Y) means X -> Y
 Output: S+ (set). This includes the original set S and additional dependencies
 """
-
+import copy
 def armstrongsAxioms(fd):
     if not isinstance(fd, list):
         raise TypeError("Expected list, got %s", type(fd))
     
-    toReturn = fd
+    toReturn = copy.deepcopy(fd)
     
     # Apply reflexivity rule
+    for rel in fd:
+        if not isinstance(rel, tuple):
+            raise TypeError("Expected tuple, got %s", type(rel))
+        if (len(rel[0]) > 1):
+            # Add additional functional dependencies to toReturn
+            listOfSubstrings = []
+            for i in range(len(rel[0])):
+                for end in range(i+1, len(rel[0])+1):
+                    listOfSubstrings.append(rel[0][i: end])
+            listOfSubstrings.remove(rel[0])
+            # Add each substring to toReturn
+            for substr in listOfSubstrings:
+                toReturn.append((rel[0], substr))
+    print(toReturn)
+armstrongsAxioms([("abc", "d")])
+                
